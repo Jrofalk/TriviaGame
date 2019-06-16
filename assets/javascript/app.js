ba8ab52c93464d8043fbdd43e58b0155957a6671
John Falk <jrfalk@outlook.com>
2019-06-16 17:49:53 -0400

    Sketched out overall game functionality, includes lorem ipsum text. Selecting true or false hides previous questions and triggers next question in sequence

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@
 
 var time = 100;
 var intervalId;
+var wins = 0;
+
 
 function run() {
     clearInterval(intervalId);
@@ -18,17 +20,50 @@ function decrement() {
 
 function stop() {
     clearInterval(intervalId);
-    }
+}
   
+
 
 
   run();
 
+
+
   $('#start').on('click', function(){
-    $('#questions').show();
+    $('.questions').show();
     $('#start').hide();
   });
 
+ $('#correct-one').on ('click', function(){
+    wins++;
+    console.log(wins);
+    $('#question-two').show();
+    $('#question-one').hide();
+  });
+
+  
+
+  $('#correct-two').on('click', function(){
+    wins++;
+    console.log(wins);
+    $('#question-three').show();
+    $('#question-two').hide();
+  });
+
+  $('#correct-three').on('click', function(){
+    wins++;
+    console.log(wins);
+    $('#question-three').hide();
+    $('#score').show()
+    $('#total-score').text(wins);
+  });
+
+  
+
+
+
+
+  
 
 
 
